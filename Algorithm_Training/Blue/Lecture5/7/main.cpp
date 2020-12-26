c21c6c640f6832d6035ca28098edfd471a2d578e
namnguyen <namnh997@gmail.com>
2020-12-26 14:49:05 +0700

    * Using run and build tool from vscode.

--- Algorithm_Training/Blue/Lecture5/7/main.cpp
+++ Algorithm_Training/Blue/Lecture5/7/main.cpp
@@ -3,6 +3,7 @@
 #define fori(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
 #define ford(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
 #define pb push_back
+#define pii pair<int, int>
 #define mp make_pair
 using namespace std;
 
@@ -17,18 +18,49 @@ void debugOut() {
 	cerr << endl;
 }
 
-void solve() {
-	que<pii> q;
+int stepX[4] = {-1, 1, 0, 0};
+int stepY[4] = {0, 0, -1, 1};
+const int MAX = 501;
+char grid[MAX][MAX];
+int n, m;
+
+
+bool BFS(pii src, pii des) {
+	queue<pii> q;
+	q.push(src);
+	grid[src.first][src.second] = 'X';
 	while (!q.empty()) {
-		pii u = q.top();
+		pii u = q.front();
 		q.pop();
-		for () {
-			if (maxtrix[x][y] == 'X' && x == r1 && y == r2) return yes;
-			else if (matrix[x][y] == '.') {
-				matrix[x][y] = 'X';
-				q.push({x, y});
+		for (int i = 0; i < 4; i++) {
+			int nextX = u.first + stepX[i];
+			int nextY = u.second + stepY[i];
+			if (nextX == des.first && nextY == des.second && grid[nextX][nextY] == 'X') return true;
+			if (nextX <= n && nextX >= 1 && nextY <= m  && nextY >= 1 && grid[nextX][nextY] == '.') {
+				grid[nextX][nextY] = 'X';
+				q.push({nextX, nextY});
 			}
 		}
+	}
+	return false;
+
+}
+
+void solve() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> grid[i][j];
+		}
+	}
+	pii src, des;
+	cin >> src.first >> src.second;
+	cin >> des.first >> des.second;
+	bool canGo = BFS(src, des);
+	if (canGo) {
+		cout << "YES";
+	} else {
+		cout << "NO";
 	}
 }
 
--- Algorithm_Training/Blue/Lecture5/8/main.cpp
+++ Algorithm_Training/Blue/Lecture5/8/main.cpp
@@ -3,6 +3,7 @@
 #define fori(i, a, b) for (int i = (a), _##i = (b); i <= _##i; ++i)
 #define ford(i, a, b) for (int i = (a), _##i = (b); i >= _##i; --i)
 #define pb push_back
+#define pii pair<int, int>
 #define mp make_pair
 using namespace std;
 
@@ -17,29 +18,79 @@ void debugOut() {
 	cerr << endl;
 }
 
-void solve() {
-	bfs(cell, s) {
-		nsheep = matrix[x][y] == 'k';
-		nwolves = matrix[x][y] == 'v';
-		queue q;
-		q.push(start);
-		canEscape = false;
-		while (!q.empty()) {
-			u = q.top(); q.pop();
-			for (v in u) {
-				if (matrix[v.row][v.col] != '#') {
-					q.push({v.row, v.col}];
+const int MAX = 251;
+int n, m;
+char matrix[MAX][MAX];
+int stepX[4] = {-1, 1, 0, 0};
+int stepY[4] = {0, 0, -1, 1};
+
+bool isValid(int row, int col) {
+	return row >= 1 && row <= n && col >= 1 && col <= m && (matrix[row][col] != '#');
+}
+
+bool isOut(int row, int col) {
+	return (row == 1 || row == n) && (col == 1 || col == m) && (matrix[row][col] != '#');
+}
+
+pii BFS(int row, int col) {
+	queue<pii> q;
+	matrix[row][col] = '#';
+	q.push({row, col});
+	int sheeps = 0, wolves = 0;
+	bool out = false;
+	while (!q.empty()) {
+		pii u = q.front(); q.pop();
+		fori (i, 0, 3) {
+			int nextX = u.first + stepX[i];
+			int nextY = u.second + stepY[i];
+			if (isValid(nextX, nextY)) {
+				if (isOut(nextX, nextY)) {
+					out = true;
 				}
+				if (matrix[nextX][nextY] == 'k') {
+					sheeps++;
+				} else if (matrix[nextX][nextY] == 'v') {
+					wolves++;
+				}
+				q.push({nextX, nextY});
+				matrix[nextX][nextY] = '#';
 			}
 		}
 	}
+	if (out) {
+		return {sheeps, wolves};
+	} else {
+		return (sheeps > wolves) ? make_pair(sheeps, 0) : make_pair(0, wolves);
+	}
 }
 
+void solve() {
+	cin >> n >> m;
+	fori (i, 1, n) {
+		fori (j, 1, m){ 
+			cin >> matrix[i][j];
+		}
+	}
+	vector<pii>  res;
+	fori (i, 1, n) {
+		fori (j, 1, m) {
+			if (matrix[i][j] == '.') {
+				pii tmp = BFS(i, j);
+				res.pb(tmp);
+			}
+		}
+	}
+	int sheeps = 0, wolves = 0;
+	for (auto &it : res) {
+		sheeps += it.first;
+		wolves += it.second;
+	}
+	cout << sheeps << " " << wolves;
+}
 
-
-int main(void){
+int main(int argc, char** argv){
 	#ifndef ONLINE_JUDGE
-	freopen("in.txt", "r", stdin);
+	freopen(argv[1], "r", stdin);
 	#endif
 	solve();
 	return 0;
